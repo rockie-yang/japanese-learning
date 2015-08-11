25ecf6b165cf358331f50272158632c324688206
Rockie Yang <rockie.yang@ericsson.com>
2015-08-11 15:23:11 +0200

    correct basic1000

--- basic1000.py
+++ basic1000.py
@@ -5,15 +5,16 @@ from unidecode import unidecode
 import codecs
 
 class Parser():
-    def __init__(self, Item):
+    def __init__(self, Item, parse_long_vowel=False):
         self.Item = Item
+        self.parse_long_vowel = parse_long_vowel
 
     def parse(self, file):
         words = []
         with codecs.open(file, 'r', 'utf-8') as f:
             for line in f.readlines():
                 try:
-                    item = self.Item(line)
+                    item = self.Item(line, self.parse_long_vowel)
                     # print(str(item))
                     words.append(item)
                 except SyntaxError as e:
@@ -26,7 +27,7 @@ class Item():
     def is_valid(items):
         return len(items) >= 4
 
-    def __init__(self, line):
+    def __init__(self, line, parse_long_vowel=False):
         self.items = line.split(';')
         if len(self.items) < 4:
             raise SyntaxError("not parsable, it should have at least 4 items split with ;")
@@ -34,7 +35,13 @@ class Item():
             self.word = self.items[0]
             self.kanji = self.items[1]
             self.english = self.items[2]
-            self.romaji = unidecode(self.items[3].strip())
+            item3 = self.items[3].strip()
+            romaji = unidecode(item3)
+            if not parse_long_vowel and romaji != item3:
+                raise SyntaxError("not parsable, it has long vowel romaji, ignore it")
+
+            # TODO all - is removed at the moment, it might need be checked
+            self.romaji = romaji.replace('-', '')
 
     def __str__(self):
         return "{0} {1} {2} {3}".format(self.word, self.romaji, self.kanji, self.english)
